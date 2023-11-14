cbd898844284b7d7f417393a73ae6debe1caf1d7
Chris Santema <chris.santema@devoteam.com>
2023-11-14 20:21:41 +0100

    sd

--- helpers/columns_helpers.py
+++ helpers/columns_helpers.py
@@ -4,7 +4,7 @@ from typing import List, Union, Optional
 import pyspark.sql.functions as F
 from pyspark.sql import DataFrame, Column
 
-#extra comment to test1
+#extra comment to test12
 
 def columns_except(df: DataFrame, ex: Optional[List[str]] = None, as_column: bool = False) -> List[Union[str, Column]]:
     """
