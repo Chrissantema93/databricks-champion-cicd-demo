f26fe1b7bb4c0ef119af8d935a145b0cac22dd41
Chris Santema <chris.santema@devoteam.com>
2023-11-15 14:59:44 +0100

    adjusted pipelines

--- terraform/github/pipeline.tf
+++ terraform/github/pipeline.tf
@@ -4,7 +4,7 @@ resource "databricks_pipeline" "databricks_champion_pipeline" {
 
 
   cluster {
-    num_workers = 0
+    num_workers = 1
     spark_conf = {
       # Single-node
       "spark.databricks.cluster.profile" : "singleNode"
@@ -28,7 +28,7 @@ resource "databricks_pipeline" "databricks_champion_pipeline_test" {
   storage = "/test/first-pipeline"
 
   cluster {
-    num_workers = 0
+    num_workers = 1
     spark_conf = {
       # Single-node
       "spark.databricks.cluster.profile" : "singleNode"
@@ -38,7 +38,11 @@ resource "databricks_pipeline" "databricks_champion_pipeline_test" {
     "ResourceClass" = "SingleNode"
   }
   }
-
+  library {
+    notebook {
+      path = "${databricks_repo.databricks_champion_repo.path}/pipelines/DLT-Pipeline"
+    }
+  }
   library {
     notebook {
       path = "${databricks_repo.databricks_champion_repo.path}/tests/integration/DLT-Pipeline-Test"
