6ae326349b3417ac7cbc2f31ac25a3ac3953faf8
Chris Santema <chris.santema@devoteam.com>
2023-11-16 15:15:32 +0100

    updated cluster

--- terraform/github/cluster.tf
+++ terraform/github/cluster.tf
@@ -1,5 +1,5 @@
-data "databricks_node_type" "smallest" {
-  local_disk = true
+data "databricks_node_type" "standard" {
+  category = "General Purpose"
 }
 
 data "databricks_spark_version" "latest_lts" {
@@ -10,7 +10,7 @@ data "databricks_spark_version" "latest_lts" {
 resource "databricks_cluster" "dlt_files_in_repos_testing" {
   cluster_name            = "DLT Files in Repos notebooks testing (${data.databricks_current_user.me.alphanumeric})"
   spark_version           = data.databricks_spark_version.latest_lts.id
-  node_type_id            = data.databricks_node_type.smallest.id
+  node_type_id            = "Standard_DS3_v2"
   autotermination_minutes = 20
   spark_conf = {
     # Single-node
