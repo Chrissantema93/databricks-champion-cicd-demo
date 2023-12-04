72f03a7dcda51b26a246f2cc2695a359b5dc9cbc
Chris Santema <chris.santema@devoteam.com>
2023-12-04 15:20:10 +0100

    da

--- tests/integration/plant_parsing.py
+++ tests/integration/plant_parsing.py
@@ -1,36 +1,46 @@
+# Databricks notebook source
+# COMMAND ----------
 from runtime.nutterfixture import NutterFixture, tag
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf, col
 from pyspark.sql.types import IntegerType
 import xml.etree.ElementTree as ET
 
+
+# COMMAND ----------
 from helpers.plants_helpers import parse_price
 from helpers.columns_helpers import *
 
+# COMMAND ----------
 class TestXmlToDataFrame(NutterFixture):
     def run_test_conversion(self):
         # Initialize Spark session
         spark = SparkSession.builder.appName("IntegrationTest").getOrCreate()
 
-        # Read the XML file
-        xml_file_path = "/FileStore/tables/plants.xml"  # Path to your XML file
-        tree = ET.parse(xml_file_path)
-        root = tree.getroot()
+        xml_file_path = "/FileStore/tables/plants.xml" 
 
-        # Convert XML to DataFrame directly
-        data = []
-        for child in root:
-            record = {elem.tag: elem.text for elem in child}
-            data.append(record)
-        sdf = spark.createDataFrame(data)
+        xml_query = f"""
+            CREATE TEMPORARY VIEW plants
+            USING xml
+            OPTIONS (path "{xml_file_path}", rowTag "plant")"""
+
+        # Read the XML file
+        df = spark.sql(xml_query)
+
 
         # Register UDF to apply parse_price function
         parse_price_udf = udf(parse_price, IntegerType())
-        sdf = sdf.withColumn("PRICE", parse_price_udf(col("PRICE")))
+        parsed_df = df.withColumn("PRICE_PARSED", parse_price_udf(col("PRICE")))
+
+        filtered_df = parsed_df.select(*dataframe_except_columns(parsed_df, ["PRICE", "AVAILABILITY"]))
+
         
         # For example, assert that all prices are non-negative
-        assert sdf.filter(col("PRICE") < 0).count() == 0, "Negative prices found in the DataFrame"
+        assert filtered_df.filter(col("PRICE_PARSED") < 0).count() == 0, "Negative prices found in the DataFrame"
+        assert "PRICE" not in filtered_df.columns, "PRICE column was not removed successfully"
+        assert "AVAILABILITY" not in filtered_df.columns, "AVAILABILITY column was not removed successfully"
 
+# COMMAND ----------
 
 # Run the test
 result = TestXmlToDataFrame().execute_tests()
